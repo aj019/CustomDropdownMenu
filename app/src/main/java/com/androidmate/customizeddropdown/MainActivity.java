761cc42abdb6fb68c07674ba7804a1154a65a323
Divya gupta <Divya gupta>
2017-01-20 16:11:08 +0530

    Initial commit

---
+++ app/src/main/java/com/androidmate/customizeddropdown/MainActivity.java
@@ -0,0 +1,50 @@
+package com.androidmate.customizeddropdown;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.androidmate.customizeddropdown.adapter.CustomSpinnerAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spCustom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinnerCustom = (Spinner) findViewById(R.id.spinnerCustom);
+        // Spinner Drop down elements
+        ArrayList<String> languages = new ArrayList<String>();
+        languages.add("Andorid");
+        languages.add("IOS");
+        languages.add("PHP");
+        languages.add("Java");
+        languages.add(".Net");
+        CustomSpinnerAdapter customSpinnerAdapter = new CustomSpinnerAdapter(MainActivity.this, languages);
+        spinnerCustom.setAdapter(customSpinnerAdapter);
+
+        spinnerCustom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Android Custom Spinner Example Output..." + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+}
